7b0a860e1ff8b548dd43856087f63d03e704d362
东杰 <dongjie.wang@tempoon.com>
2018-09-28 20:46:12 +0800

    增加404页面

--- docs/.vuepress/theme/NotFound.vue
+++ docs/.vuepress/theme/NotFound.vue
@@ -1,26 +1,40 @@
 <template>
-  <div class="theme-container">
+  <div class="page-404">
     <div class="content">
-      <h1>404</h1>
-      <blockquote>{{ getMsg() }}</blockquote>
-      <router-link to="/">Take me home.</router-link>
+      <img class="404-img" src="/404.png" />
+      <div class="tips">对不起，页面迷路了，我们建议您...</div>
+      <router-link to="/" class="back-home">返回首页</router-link>
     </div>
   </div>
 </template>
 
-<script>
-const msgs = [
-  `There's nothing here.`,
-  `How did we get here?`,
-  `That's a Four-Oh-Four.`,
-  `Looks like we've got some broken links.`
-]
-
-export default {
-  methods: {
-    getMsg () {
-      return msgs[Math.floor(Math.random() * msgs.length)]
-    }
+<style lang="stylus">
+.page-404{
+  width 100vw
+  height 100vh
+  background #ffffff
+  .tips{
+    color #666666
+    font-size 24px
+    text-align center
+  }
+  .back-home{
+    width 200px
+    height 50px
+    border-radius 25px
+    background #20a0ff
+    border 2px solid #eee
+    color #ffffff
+    text-align center
+    line-height 50px
+    display block
+    font-size 20px
+    margin 20px auto
+    cursor pointer
+    text-decoration none
   }
 }
-</script>
+
+@media (max-width: $MQMobile)
+  .page-404{}
+</style>
